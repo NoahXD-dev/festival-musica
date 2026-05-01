93e9683d3ce2ca3efcb8002b6242086953c73b4e
Noe Ramirez Castro <ramcas.noe@gmail.com>
2026-05-01 10:23:03 -0600

    Update Gulp tasks and file paths for improved asset management
    - Refactored Gulp tasks to include new tasks for processing images and videos.
    - Updated file paths in index.html and app.js to reflect changes in the directory structure.
    - Modified the default Gulp task to include the new build process for images and videos.
    - Adjusted .gitignore to exclude .mp4 files from tracking.

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,7 @@ function crearGaleria() {
 
 function mostrarImg(i) {
     const imagen = document.createElement('IMG');
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.src = `img/gallery/full/${i}.jpg`;
     imagen.alt = 'Imagen galeria';
 
     const modal = document.createElement('DIV');
