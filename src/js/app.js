eefadd602bb5ab6093d47c7bdc7d82d36e1cce9a
Noe Ramirez Castro <ramcas.noe@gmail.com>
2026-05-01 09:07:52 -0600

    Add modal functionality for image gallery with styling enhancements
    - Implemented modal display for images in the gallery, allowing users to view images in a larger format.
    - Added event handlers for image clicks to trigger the modal.
    - Introduced styles for the modal, including fade-in and fade-out animations.
    - Updated global styles to manage overflow when the modal is active.

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,48 @@ function crearGaleria() {
 
     for (let i = 1; i <= CANTIDAD_IMG; i++) {
         const imagen = document.createElement('IMG');
+        imagen.src = `src/img/gallery/full/${i}.jpg`;
+        imagen.alt = 'Imagen galeria';
 
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen galeria'
+        // Event handler
+        imagen.onclick = function() {
+            mostrarImg(i);
+        }
 
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImg(i) {
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'Imagen galeria';
+
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+    modal.onclick = cerrarModal;
+
+    const btnCerrar = document.createElement('BUTTON');
+    btnCerrar.textContent = 'X';
+    btnCerrar.classList.add('btn-cerrar');
+    btnCerrar.onclick = cerrarModal;
+
+    modal.appendChild(imagen);
+    modal.appendChild(btnCerrar);
+
+    const body = document.querySelector('body');
+    body.classList.add('overFlow-hiden');
+    body.appendChild(modal);
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out');
+
+    setTimeout(() => {
+        modal?.remove();
+
+        const body = document.querySelector('body');
+        body.classList.remove('overFlow-hiden');
+    }, 500);
 }
