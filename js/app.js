59e3ea8e8d42f8d5c6ddf2bbe4c5db275985a2c5
Noe Ramirez Castro <ramcas.noe@gmail.com>
2026-05-01 20:53:27 -0600

    release: v1.0.1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-const GALERIA_CLOUDINARY_URLS = [null, "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659271/festival_musica/src/img/gallery/full/1.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659271/festival_musica/src/img/gallery/full/2.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659278/festival_musica/src/img/gallery/full/3.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659279/festival_musica/src/img/gallery/full/4.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659280/festival_musica/src/img/gallery/full/5.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659281/festival_musica/src/img/gallery/full/6.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659282/festival_musica/src/img/gallery/full/7.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659283/festival_musica/src/img/gallery/full/8.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659285/festival_musica/src/img/gallery/full/9.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659286/festival_musica/src/img/gallery/full/10.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659287/festival_musica/src/img/gallery/full/11.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659289/festival_musica/src/img/gallery/full/12.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659290/festival_musica/src/img/gallery/full/13.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659291/festival_musica/src/img/gallery/full/14.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659292/festival_musica/src/img/gallery/full/15.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777659293/festival_musica/src/img/gallery/full/16.jpg"];
-
+const GALERIA_THUMB_URLS_AVIF = [null, "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690085/festival_musica/build/img/gallery/thumb/1.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690086/festival_musica/build/img/gallery/thumb/2.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690087/festival_musica/build/img/gallery/thumb/3.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690088/festival_musica/build/img/gallery/thumb/4.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690089/festival_musica/build/img/gallery/thumb/5.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690091/festival_musica/build/img/gallery/thumb/6.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690092/festival_musica/build/img/gallery/thumb/7.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690092/festival_musica/build/img/gallery/thumb/8.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690094/festival_musica/build/img/gallery/thumb/9.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690095/festival_musica/build/img/gallery/thumb/10.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690096/festival_musica/build/img/gallery/thumb/11.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690096/festival_musica/build/img/gallery/thumb/12.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690097/festival_musica/build/img/gallery/thumb/13.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690098/festival_musica/build/img/gallery/thumb/14.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690099/festival_musica/build/img/gallery/thumb/15.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690100/festival_musica/build/img/gallery/thumb/16.avif.avif"];
+const GALERIA_THUMB_URLS_WEBP = [null, "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690100/festival_musica/build/img/gallery/thumb/1.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690101/festival_musica/build/img/gallery/thumb/2.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690102/festival_musica/build/img/gallery/thumb/3.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690103/festival_musica/build/img/gallery/thumb/4.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690105/festival_musica/build/img/gallery/thumb/5.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690106/festival_musica/build/img/gallery/thumb/6.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690107/festival_musica/build/img/gallery/thumb/7.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690108/festival_musica/build/img/gallery/thumb/8.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690109/festival_musica/build/img/gallery/thumb/9.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690110/festival_musica/build/img/gallery/thumb/10.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690111/festival_musica/build/img/gallery/thumb/11.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690112/festival_musica/build/img/gallery/thumb/12.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690112/festival_musica/build/img/gallery/thumb/13.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690113/festival_musica/build/img/gallery/thumb/14.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690114/festival_musica/build/img/gallery/thumb/15.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690115/festival_musica/build/img/gallery/thumb/16.webp.webp"];
+const GALERIA_THUMB_URLS_JPG = [null, "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690116/festival_musica/src/img/gallery/thumb/1.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690117/festival_musica/src/img/gallery/thumb/2.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690117/festival_musica/src/img/gallery/thumb/3.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690118/festival_musica/src/img/gallery/thumb/4.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690119/festival_musica/src/img/gallery/thumb/5.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690120/festival_musica/src/img/gallery/thumb/6.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690121/festival_musica/src/img/gallery/thumb/7.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690121/festival_musica/src/img/gallery/thumb/8.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690123/festival_musica/src/img/gallery/thumb/9.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690124/festival_musica/src/img/gallery/thumb/10.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690125/festival_musica/src/img/gallery/thumb/11.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690126/festival_musica/src/img/gallery/thumb/12.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690127/festival_musica/src/img/gallery/thumb/13.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690128/festival_musica/src/img/gallery/thumb/14.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690128/festival_musica/src/img/gallery/thumb/15.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690130/festival_musica/src/img/gallery/thumb/16.jpg.jpg"];
+const GALERIA_FULL_URLS_AVIF = [null, "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690130/festival_musica/build/img/gallery/full/1.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690131/festival_musica/build/img/gallery/full/2.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690132/festival_musica/build/img/gallery/full/3.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690133/festival_musica/build/img/gallery/full/4.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690133/festival_musica/build/img/gallery/full/5.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690134/festival_musica/build/img/gallery/full/6.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690135/festival_musica/build/img/gallery/full/7.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690136/festival_musica/build/img/gallery/full/8.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690137/festival_musica/build/img/gallery/full/9.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690138/festival_musica/build/img/gallery/full/10.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690139/festival_musica/build/img/gallery/full/11.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690140/festival_musica/build/img/gallery/full/12.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690141/festival_musica/build/img/gallery/full/13.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690142/festival_musica/build/img/gallery/full/14.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690143/festival_musica/build/img/gallery/full/15.avif.avif", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690144/festival_musica/build/img/gallery/full/16.avif.avif"];
+const GALERIA_FULL_URLS_WEBP = [null, "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690144/festival_musica/build/img/gallery/full/1.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690145/festival_musica/build/img/gallery/full/2.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690146/festival_musica/build/img/gallery/full/3.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690148/festival_musica/build/img/gallery/full/4.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690148/festival_musica/build/img/gallery/full/5.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690149/festival_musica/build/img/gallery/full/6.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690150/festival_musica/build/img/gallery/full/7.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690151/festival_musica/build/img/gallery/full/8.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690152/festival_musica/build/img/gallery/full/9.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690153/festival_musica/build/img/gallery/full/10.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690154/festival_musica/build/img/gallery/full/11.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690157/festival_musica/build/img/gallery/full/12.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690158/festival_musica/build/img/gallery/full/13.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690160/festival_musica/build/img/gallery/full/14.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690160/festival_musica/build/img/gallery/full/15.webp.webp", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690161/festival_musica/build/img/gallery/full/16.webp.webp"];
+const GALERIA_FULL_URLS_JPG = [null, "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690163/festival_musica/src/img/gallery/full/1.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690165/festival_musica/src/img/gallery/full/2.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690168/festival_musica/src/img/gallery/full/3.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690170/festival_musica/src/img/gallery/full/4.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690172/festival_musica/src/img/gallery/full/5.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690173/festival_musica/src/img/gallery/full/6.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690175/festival_musica/src/img/gallery/full/7.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690176/festival_musica/src/img/gallery/full/8.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690177/festival_musica/src/img/gallery/full/9.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690179/festival_musica/src/img/gallery/full/10.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690180/festival_musica/src/img/gallery/full/11.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690182/festival_musica/src/img/gallery/full/12.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690183/festival_musica/src/img/gallery/full/13.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690184/festival_musica/src/img/gallery/full/14.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690186/festival_musica/src/img/gallery/full/15.jpg.jpg", "https://res.cloudinary.com/di12fiqfa/image/upload/v1777690188/festival_musica/src/img/gallery/full/16.jpg.jpg"];
 document.addEventListener('DOMContentLoaded', function() {
     navegacionFija();
     crearGaleria();
@@ -25,23 +29,53 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
     for (let i = 1; i <= CANTIDAD_IMG; i++) {
-        const imagen = document.createElement('IMG');
-        imagen.src = (GALERIA_CLOUDINARY_URLS[i] || `img/gallery/full/${i}.jpg`);
+        const picture = document.createElement('picture');
+
+        const sourceAvif = document.createElement('source');
+        sourceAvif.type = 'image/avif';
+        sourceAvif.srcset = (GALERIA_THUMB_URLS_AVIF[i] || `img/gallery/thumb/${i}.avif`);
+
+        const sourceWebp = document.createElement('source');
+        sourceWebp.type = 'image/webp';
+        sourceWebp.srcset = (GALERIA_THUMB_URLS_WEBP[i] || `img/gallery/thumb/${i}.webp`);
+
+        const imagen = document.createElement('img');
+        imagen.loading = 'lazy';
+        imagen.width = '300';
+        imagen.height = '200';
+        imagen.src = (GALERIA_THUMB_URLS_JPG[i] || `img/gallery/thumb/${i}.jpg`);
         imagen.alt = 'Imagen galeria';
 
-        // Event handler
+        picture.appendChild(sourceAvif);
+        picture.appendChild(sourceWebp);
+        picture.appendChild(imagen);
+
         imagen.onclick = function() {
             mostrarImg(i);
-        }
+        };
 
-        galeria.appendChild(imagen);
+        galeria.appendChild(picture);
     }
 }
 
 function mostrarImg(i) {
-    const imagen = document.createElement('IMG');
-    imagen.src = (GALERIA_CLOUDINARY_URLS[i] || `img/gallery/full/${i}.jpg`);
+    const picture = document.createElement('picture');
+
+    const sourceAvif = document.createElement('source');
+    sourceAvif.type = 'image/avif';
+    sourceAvif.srcset = (GALERIA_FULL_URLS_AVIF[i] || `img/gallery/full/${i}.avif`);
+
+    const sourceWebp = document.createElement('source');
+    sourceWebp.type = 'image/webp';
+    sourceWebp.srcset = (GALERIA_FULL_URLS_WEBP[i] || `img/gallery/full/${i}.webp`);
+
+    const imagen = document.createElement('img');
+    imagen.src = (GALERIA_FULL_URLS_JPG[i] || `img/gallery/full/${i}.jpg`);
     imagen.alt = 'Imagen galeria';
+
+    picture.appendChild(sourceAvif);
+    picture.appendChild(sourceWebp);
+    picture.appendChild(imagen);
 
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
@@ -52,7 +86,7 @@ function mostrarImg(i) {
     btnCerrar.classList.add('btn-cerrar');
     btnCerrar.onclick = cerrarModal;
 
-    modal.appendChild(imagen);
+    modal.appendChild(picture);
     modal.appendChild(btnCerrar);
 
     const body = document.querySelector('body');
